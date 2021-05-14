11dd3df6d0aeed53272a6d5eb5a8a674cfbf4c7a
yezii <1126yezy@gmail.com>
2021-05-14 15:55:22 +0900

    Classes on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "yeji",
-  age: 25,
-  gender: "female",
-};
+const yeji = new Human("Yeji", 25, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age} years old and you are a ${person.gender}.`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(yeji));
 
 export {};
