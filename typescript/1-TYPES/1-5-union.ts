8b0e949950bb0ddc1140fe792e878cc25deb6c10
yezii <1126yezy@gmail.com>
2021-05-28 01:24:43 +0900

    Union Types

---
+++ typescript/1-TYPES/1-5-union.ts
@@ -0,0 +1,41 @@
+{
+  // Union Types: OR
+
+  type Direction = "left" | "right" | "up" | "down";
+  function move(direction: Direction) {
+    console.log(direction);
+  }
+  move("down");
+
+  type TileSize = 8 | 16 | 32;
+  const tile: TileSize = 16;
+
+  type SuccessState = {
+    response: {
+      body: string;
+    };
+  };
+  type FailState = {
+    reason: string;
+  };
+  type LoginState = SuccessState | FailState;
+  function login(): LoginState {
+    return {
+      response: {
+        body: "logged in !!",
+      },
+    };
+  }
+
+  // printLoginState(state: LoginState)
+  // success -> 🎊 body
+  // fail -> 😭 reason
+
+  function printLoginState(state: LoginState) {
+    if ("response" in state) {
+      console.log(`🎊 ${state.response.body}`);
+    } else {
+      console.log(`😭 ${state.reason}`);
+    }
+  }
+}
